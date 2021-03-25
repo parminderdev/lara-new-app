8b3d17f4c38a3f1bb323004d84a5be18e66288c5
parminderdev <75047076+parminderdev@users.noreply.github.com>
2021-03-25 20:20:16 +1300

    Retrieving Or Creating Models
    I learned how you can insert, update, delete new data in database tables using the model. 
    I did migration using PHP artisan as well.
    Tested lots of laravel coding structures

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+
 class UserController extends Controller
 {
     //
     public function index()
     {
-        # code...
-        return 'This is from the User Controller';
+        // $user = new User();
+        // var_dump($user);
+        // DB::insert('insert into users ( name,email,password) values (?, ?,?)', ['Marc3','parm3@demo.com','pinka']);
+        // # code...
+
+        $user = User::all();
+        //return $user;
+       $deletedRows = User::where('id', 2)->delete();
+
+       $data = [
+        'name' => 'parminder12',
+        'email' => 'parm85@demo.com',
+        'password' => '154641'
+
+       ];
+
+       User::create($data);
+
+       $user1 = User::create([
+        'name' => 'Taylor',
+        'email' => 'Otwell',
+        'password' => bcrypt('Developer'),
+    ]);
+
+        foreach ($user as $U) {
+            echo  $U->name;
+        }
+        //return 'This is from the User Controller ! Entry inserted successfully';
     }
 }
